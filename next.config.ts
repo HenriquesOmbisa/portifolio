a1b8dff333aba96ef197ac7615d0aa97fdfdb01d
Henriques Ombisa <henriquesombisa@gmail.com>
2025-10-02 23:59:16 +0100

    fix midia display in next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: "export",
+  basePath: "/portifolio",
+  assetPrefix: "/portifolio/",
 };
 
 export default nextConfig;
